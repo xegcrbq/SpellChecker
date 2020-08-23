3b55656fd3a6fa767c1a52b8f1c1cb3167ba3e0e
Роман Смагин <Romayx@yandex.ru>
2020-08-23 13:15:01 +0300

    Created

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpellChecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            string fileText ="";
+            string[] fileSplittedText;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                fileText = File.ReadAllText(openFileDialog.FileName);
+            fileText = fileText.Replace("\r", "");
+            fileSplittedText = fileText.Split("===");
+            if (fileSplittedText.Length == 3)
+            {
+                dictionary.Text = (fileSplittedText[0].Last() == '\n') ? fileSplittedText[0].Remove(fileSplittedText[0].Length - 1, 1) : fileSplittedText[0];
+                textData.Text = (fileSplittedText[1].Last() == '\n') ? fileSplittedText[1].Remove(fileSplittedText[1].Length - 1, 1) : fileSplittedText[1];
+                if (textData.Text[0] == '\n')
+                    textData.Text = textData.Text.Remove(0, 1);
+            }
+            else
+            {
+                dictionary.Text = "incorrect input file!";
+                textData.Text = "incorrect input file!";
+            }
+                
+            output.Text = "";
+
+
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            Spelling sp = new Spelling(dictionary.Text);
+            output.Text = sp.Start(textData.Text);
+        }
+    }
+}
---
+++ Spelling.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Media.TextFormatting;
+
+namespace SpellChecker
+{
+    enum prevEdit { none, insert, delete };
+    class Spelling
+    {        
+        private Dictionary<String, int> _dictionary = new Dictionary<string, int>();
+        private static Regex _wordRegex = new Regex("[a-z]+", RegexOptions.Compiled);
+
+        public Spelling(string inputDictionary )
+        {
+            InitDictionary(inputDictionary);            
+        }
+
+        public string Start(string inputText)
+        {
+            return CorrectText(inputText);           
+        }
+        private string CorrectWord(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+                return word;
+            word = word.ToLower();
+            if (_dictionary.ContainsKey(word))
+                return word;
+            //search depth 1
+            List<Tuple<string, prevEdit, int>> listFromEdits = WordVariations(word);
+            string result = WordCorrectionResults(listFromEdits);
+            if (result.Any()) return result;
+            //search depth 2
+            listFromEdits = MultipleWordsVariations(listFromEdits);
+            result = WordCorrectionResults(listFromEdits);
+            if (result.Any()) return result;
+            //word not found
+            return "{" + word + "?}";
+                
+        }
+
+        private List<Tuple<string, prevEdit, int>> WordVariations(string word, prevEdit prevE = 0, int editPos = -1)
+        {
+            List<Tuple<string, string>> splits = new List<Tuple<string, string>>();
+            List<Tuple<string, prevEdit, int>> edits = new List<Tuple<string, prevEdit, int>>();
+            for(int i = 0; i < word.Length; i++)
+            {
+                var tuple = new Tuple<string, string>(word.Substring(0, i), word.Substring(i));
+                splits.Add(tuple);
+            }
+            //insert
+            for(int i = 0; i < splits.Count; i++)
+            {
+                if (prevE == (prevEdit)1 && (i == editPos || i == editPos + 1))//adjacent char check
+                    break;
+                string a = splits[i].Item1;
+                string b = splits[i].Item2;
+                for(char c = 'a'; c <='z'; c++)
+                {
+                    var tuple = new Tuple<string, prevEdit, int>(a+c+b,(prevEdit)1, a.Length);
+                    edits.Add(tuple);
+                }  
+            }
+
+            //delete
+            for(int i = 0; i < splits.Count; i++)
+            {
+                if (prevE == (prevEdit)2 && (i == editPos || i == editPos - 1))//adjacent char check
+                    break;
+                string a = splits[i].Item1;
+                string b = splits[i].Item2;
+                if (!string.IsNullOrEmpty(b))
+                {
+                    var tuple = new Tuple<string, prevEdit, int>(a + b.Substring(1), (prevEdit)2, a.Length);
+                    edits.Add(tuple);
+                }
+            }
+            return edits;
+
+        }
+
+        private List<Tuple<string, prevEdit, int>> MultipleWordsVariations(List<Tuple<string, prevEdit, int>> inputWordList)
+        {
+            List<Tuple<string, prevEdit, int>> edits = new List<Tuple<string, prevEdit, int>>();
+            foreach(var word in inputWordList)
+                edits.AddRange(WordVariations(word.Item1, word.Item2, word.Item3));
+            return edits;
+
+        }
+        private void InitDictionary(string inputDictionary)
+        {
+            inputDictionary = inputDictionary.Replace(" ", "\n");
+            List<string> dictList = inputDictionary.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+            _dictionary.Clear();
+            foreach (string word in dictList)
+            {
+                string trimmedWord = word.Trim().ToLower();
+                if (_wordRegex.IsMatch(trimmedWord))
+                {
+                    if (_dictionary.ContainsKey(trimmedWord))
+                        _dictionary[trimmedWord]++;
+                    else
+                        _dictionary.Add(trimmedWord, 1);
+                }
+            }
+        }
+
+        private string CorrectText(string inputText)
+        {
+            string output;
+            List<string> wordList = inputText.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            output = "";
+            foreach (string word in wordList)
+            {
+                if (word.Contains('\n'))
+                    output += CorrectWord(word.Substring(0, word.LastIndexOf('\n')))
+                        + '\n' + CorrectWord(word.Substring(word.LastIndexOf('\n') + 1)) + " ";
+                else
+                    output += CorrectWord(word) + " ";
+            }
+            if(output.Contains(' ')) output = output.Remove(output.LastIndexOf(' '));
+            return output;
+        }
+        
+        private string WordCorrectionResults(List<Tuple<string, prevEdit, int>> listFromEdits)
+        {
+            Dictionary<string, int> candidates = new Dictionary<string, int>();
+
+            for (int i = 0; i < listFromEdits.Count; i++)
+            {
+                if (_dictionary.ContainsKey(listFromEdits[i].Item1) && !candidates.ContainsKey(listFromEdits[i].Item1))
+                    candidates.Add(listFromEdits[i].Item1, _dictionary[listFromEdits[i].Item1]);
+            }
+            if (candidates.Count == 1)
+                return candidates.First().Key;
+            if (candidates.Count > 1)
+            {
+                string result = "{";
+                foreach (var candidate in candidates)
+                    result += candidate.Key + " ";
+                result = result.Remove(result.Length - 1) + "}";
+                result = result.Replace(" ", ", ");
+                return result;
+            }
+            return "";
+        }
+    }
+}
